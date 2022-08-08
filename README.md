552f0a7fa82e3d340f3f9a682de6b7eaf9bbcb09
Carol <carolinemaee@live.com>
2022-08-08 11:58:11 +0930

    updated code

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# Professional README Generator Starter Code
-
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+# Readme File Generator
+## Description
+Generate a README.md file
+## Table of Contents
+1. [Installation](#installation)
+1. [Usage](#usage)
+1. [License](#license)
+1. [Contribution](#contribution)
+1. [Testing](#testing)
+1. [Questions](#questions)
+## Installation
+To run this application, you must run command 'npm i' to install dependencies.
+## Usage
+Follow and answer the prompts to generate a README file with the user input
+## License
+The product is licensed under the MIT license.
+## Contribution
+User can contribute by creating pull request for approval
+## Testing
+To test the application, you must run the 'npm test' command.
+## Screenshots
+![Screenshot 01](./images/screenshot-01.jpg)  
+Install dependencies  
+  
+![Screenshot 02](./images/screenshot-02.jpg)  
+Using the application  
+## Questions
+### Where can I find the repo?
+Follow the link: [https://github.com/carolinemae](https://github.com/carolinemae)
+### How can I contact the creator if I have questions?
+Contact via email: [carolinemaee@live.com](mailto:carolinemaee@live.com)
--- index.js
+++ index.js
@@ -1,13 +1,112 @@
-// TODO: Include packages needed for this application
+// Packages required for application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Function to generate the license badge
+function generateLicense(license) {
+    let licenseBadge = {
+        MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+        APACHE: '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+        GPL: '[![License: GPL 3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+        BSD: '[![License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+        None: ''
+    }
+    // Return license badge dependent on user selection
+    return licenseBadge[license.split(" ")[0]];
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Generate the README.md file and populate with user input from Inquirer
+const gennerateReadme = ({projectTitle,githubUser,description,installDep,usageInstruct,license,repoContrib,testCommand,email}) =>
+`# ${projectTitle}
+${generateLicense(license)}
+## Description
+${description}
+## Table of Contents
+1. [Installation](#installation)
+1. [Usage](#usage)
+1. [License](#license)
+1. [Contribution](#contribution)
+1. [Testing](#testing)
+1. [Questions](#questions)
+## Installation
+To run this application, you must run command '${installDep}' to install dependencies.
+## Usage
+${usageInstruct}
+## License
+The product is licensed under the ${license} license.
+## Contribution
+${repoContrib}
+## Testing
+To test the application, you must run the '${testCommand}' command.
+## Questions
+### Where can I find the repo?
+Follow the link: [https://github.com/${githubUser}](https://github.com/${githubUser})
+### How can I contact the creator if I have questions?
+Contact via email: [${email}](mailto:${email})`;
 
-// TODO: Create a function to initialize app
-function init() {}
+// Inquirer questions to ask user
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'githubUser',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is your project called?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description for your project:',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None'],
+        },
+        {
+            type: 'input',
+            name: 'installDep',
+            message: 'What command should be run to install dependencies?',
+            default: 'npm i',
+        },
+        {
+            type: 'input',
+            name: 'testCommand',
+            message: 'What command should be run to run tests?',
+            default: 'npm test',
+        },
+        {
+            type: 'input',
+            name: 'usageInstruct',
+            message: 'How do you use your application?',
+        },
+        {
+            type: 'input',
+            name: 'repoInstruct',
+            message: 'What does the user need to know about using the repo?',
+        },
+        {
+            type: 'input',
+            name: 'repoContrib',
+            message: 'What does the user need to know about contributing to the repo?',
+        },
+    ])
+    // After entering all of users input
+    .then((answers) => {
 
-// Function call to initialize app
-init();
+        const readmeContent = gennerateReadme(answers);
+
+        fs.writeFile('README.md', readmeContent, (err) =>
+        err ? console.log(err) : console.log('Successfully generated README file')
+        );
+    });
